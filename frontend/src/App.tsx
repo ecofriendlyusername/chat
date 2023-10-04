9c94acfdf7e65de755cfa663d97c12d0dd77e15e
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-10-04 23:30:05 +0900

    feat: 초기 라우터 설정완료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,22 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Login from './pages/Login';
+import Room from './pages/Room';
+import RoomSearch from './pages/RoomSearch';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="login" element={<Login />} />
+        <Route path="room" element={<Room />} />
+        <Route path="roomsearch" element={<RoomSearch />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/pages/Main/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Main() {
+  return <div>main페이지에요</div>;
+}
