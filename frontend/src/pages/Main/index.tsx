55c33beca636aeb0554ed1e6a8637dd207d4827c
HyeseungNA <nahs00122@gmail.com>
2023-10-31 21:20:23 +0900

    feat: 로그인 쿠키

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { useCookies } from 'react-cookie';
+import { Cookies } from 'react-cookie';
 
 export default function Main() {
-  const [cookies, setCookie] = useCookies(['name']);
-  useEffect (() => {
-    console.log(cookies)
-    
-  },[])
+  const cookies = new Cookies();
 
-  if (cookies.name === null)
-    return (
-      <div>
-        <a href="http://localhost:8081/oauth2/authorization/google">
-          click here
-        </a>
-      </div>
-    );
-
+  const check = (): void => {
+    const jsessionid = cookies.get('JSESSIONID');
+    console.log(jsessionid);
+  }
   return (
     <div>
-      {cookies.name}
-      <h1>main페이지</h1>
+      {cookies.get('JSESSIONID') ? (
+        <h1>main 페이지</h1>
+      ) : (
+        <div>
+          <a href="http://localhost:8081/oauth2/authorization/google">
+            click here
+          </a>
+        </div>
+      )}
+      <button onClick={() => check()}>쿠키 확인</button>
     </div>
-  );
+  )
 }
