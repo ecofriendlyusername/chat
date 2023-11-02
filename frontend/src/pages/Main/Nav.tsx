1af501c4a848ad7f2a53d9f06ad11892632d33cf
HyeseungNA <nahs00122@gmail.com>
2023-11-02 10:53:21 +0900

    style: user nav css 틀 구축

--- frontend/src/pages/Main/Nav.tsx
+++ frontend/src/pages/Main/Nav.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import Profile from '../../Images/profile.png';
+import { BsPlusCircleFill } from 'react-icons/bs';
 
 export default function Nav() {
+  interface User {
+    name: string;
+    src: string;
+  }
+  const users: User[] = [
+    { name: '가영', src: Profile },
+    { name: '동민', src: Profile },
+    { name: '혜승', src: Profile },
+    { name: '쿠키', src: Profile },
+  ];
   return (
-    <div className="h-[10vh] w-[100vw] bg-brand-pink">
+    <div className="h-[13vh] w-[100vw] bg-brand-pink">
       <div className="flex">
-        <img src={Profile} alt="프로필 사진" className="w-[6rem] ml-5" />
-        <img src={Profile} alt="프로필 사진" className="w-[6rem] ml-5" />
-        <img src={Profile} alt="프로필 사진" className="w-[6rem] ml-5" />
-        <img src={Profile} alt="프로필 사진" className="w-[6rem] ml-5" />
-        <img src={Profile} alt="프로필 사진" className="w-[6rem] ml-5" />
-        <img src={Profile} alt="프로필 사진" className="w-[6rem] ml-5" />
+        {users.map((user, index) => (
+          <div className=" mx-6" key={index}>
+            <img src={user.src} className="h-[6rem]" alt="프로필 사진" />
+            <p className="text-center mt-1 font-CookieRun_Regular">
+              {user.name}
+            </p>
+          </div>
+        ))}
+        <BsPlusCircleFill size={90} className="opacity-70 ml-6 mt-2" />
+        <div className="flex ml-auto">
+          <div className="flex mr-6 flex-col">
+            <img src={Profile} className="h-[6rem]" alt="내 프로필" />
+            <p className="text-center mt-1 font-CookieRun_Regular">나</p>
+          </div>
+        </div>
       </div>
-      <div></div>
     </div>
   );
 }
