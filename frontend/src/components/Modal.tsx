a9da17019c9c91db7a07407dae42a572b1165079
HyeseungNA <nahs00122@gmail.com>
2023-11-07 19:32:12 +0900

    feat: file upload 기능 구현

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,8 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './Modal.module.css';
 import { AiOutlineClose } from 'react-icons/ai';
 import { BsCameraFill } from 'react-icons/bs';
 import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { useSubmit } from 'react-router-dom';
 
 interface ModalProps {
   close: () => void;
@@ -10,6 +11,7 @@ interface ModalProps {
 
 export default function Modal({ close }: ModalProps) {
   const outside = useRef<HTMLDivElement>(null);
+  const [isPublic, setIsPublic] = useState<boolean>(false); // 공개, 비공개
   return (
     <>
       <div
@@ -34,18 +36,31 @@ export default function Modal({ close }: ModalProps) {
               style={{ width: '15vw', height: '4vh', borderRadius: '10px' }}
               className="font-CookieRun_Regular flex-center mt-4"
             />
-            <div
+            <label
+              htmlFor="input-file"
               style={{
                 background: '#F0F1F4',
                 width: '15vw',
                 height: '25vh',
                 marginTop: '15px',
+                zIndex: 1,
               }}
               className="flex-col flex items-center justify-center"
             >
               <BsCameraFill size="100" style={{ color: '#AB8E8E' }} />
+              <input
+                type="file"
+                id="input-file"
+                accept=".png, .jpg, image"
+                style={{
+                  display: 'none',
+                  width: '0',
+                  height: '0',
+                  overflow: 'hidden',
+                }}
+              />
               <p className="font-CookieRun_Regular">사진을 넣어주세요</p>
-            </div>
+            </label>
             <div className="flex mt-4">
               <input
                 placeholder="초대할 이메일 입력"
@@ -59,12 +74,24 @@ export default function Modal({ close }: ModalProps) {
             </div>
           </div>
           <div className="flex items-center justify-center mt-[5vh]">
-            <div
-              className="font-CookieRun_Regular w-[3vw] h-[4vh] flex items-center justify-center ml-auto mr-3 rounded-md"
-              style={{ background: '#E7CFC4' }}
-            >
-              공개
-            </div>
+            {isPublic ? (
+              <div
+                className="font-CookieRun_Regular w-[3vw] h-[4vh] flex items-center justify-center ml-auto mr-3 rounded-md"
+                style={{ background: '#E7CFC4' }}
+                onClick={() => setIsPublic(false)}
+              >
+                공개
+              </div>
+            ) : (
+              <div
+                className="font-CookieRun_Regular w-[3vw] h-[4vh] flex items-center justify-center ml-auto mr-3 rounded-md"
+                style={{ background: '#F0F1F4' }}
+                onClick={() => setIsPublic(true)}
+              >
+                비공개
+              </div>
+            )}
+
             <div
               className="font-CookieRun_Regular w-[10vw] h-[5vh] flex items-center justify-center mr-[7vw] rounded-md"
               style={{ background: '#E7CFC4' }}
